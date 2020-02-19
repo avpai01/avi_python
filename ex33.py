6e0ed40c4c5bc82e01c061ca86cb0a1d6ed5432e
avpai01 <avpai01@gmail.com>
2020-02-20 01:52:57 +0530

    Added new exercises

---
+++ ex33.py
@@ -0,0 +1,17 @@
+i = 0
+numbers = []
+
+a = int(input('--->'))
+while i < a:
+	print(f"At the top i is {i}")
+	numbers.append(i)
+
+	i += 1
+	print("Numbers now: ",numbers)
+	print(f"At the botton i is {i}")
+
+
+print("The numbers:")
+
+for num in numbers:
+	print(num)
---
+++ ex34.py
@@ -0,0 +1,7 @@
+animals = ["bear", "python", 'Peacock', 'kangaroo', 'tiger', 'elephant']
+
+print(animals.index('bear'))
+
+for i in animals:
+	print(i)
+	print(animals.index(i))
